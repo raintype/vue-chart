1619692bdce928b1736c8f9bd35e889ffd4a0030
raintype <raintype@gmail.com>
2022-10-17 12:32:31 +0900

    #17-1

---
+++ chart-lib/src/components/CheckBox.vue
@@ -0,0 +1,18 @@
+<template>    
+    <div>
+        <input type="checkbox" 
+        :value="value" @click="toggleCheckBox">
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    methods: {
+        toggleCheckBox() {
+            this.$emit('input', !this.value);
+        }
+    }
+    
+}
+</script>
